2fe6ea63b7621dd00c4436b9081bb7b1cfb6fd02
Pablo Serrano <p.serrano@devel.es>
2015-02-20 18:19:57 +0100

    NOTICKET-first base of angular

--- assets/javascripts/snd-app.js
+++ assets/javascripts/snd-app.js
@@ -1,28 +1,11 @@
-/**
- * Created by pabloserrano on 20/02/15.
- */
-angular.module('single-neuron-app', [])
-    .controller('TodoController', ['$scope', function($scope){
-        $scope.todos = [
-            {text: 'Primer elemento', done: true},
-            {text: 'Segundo elemento', done: false},
+angular.module('sndapp', [])
+    .controller('ListController', ['$scope', function ($scope) {
+        $scope.apps = [
+            {name: 'SkyeApp', status: true},
+            {name: 'SkyeUm', status: false}
         ];
-        $scope.remaining = function(){
-            var count = 0;
-            angular.forEach($scope.todos, function(todo){
-                count += todo.done ? 0 : 1;
-            });
-        };
-        $scope.addTodo = function(){
-            $scope.todos.push({text:$scope.todoText, done: false});
-            $scope.todoText = '';
-        };
-        $scope.archive = function(){
-            var oldTodos = $scope.todos;
-            $scope.todos = [];
-            angular.forEach(oldTodos, function(todo){
-                if(!todo.done) $scope.todos.push(todo);
-            });
+        $scope.giveList = function () {
+            alert('Giving');
         };
 
     }]);
